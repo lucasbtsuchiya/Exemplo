454855c8fa72920ba1b646db0cbe45cc37e5c684
Lucas Tsuchiya <lucasbtsuchiya@gmail.com>
2019-03-21 20:07:13 -0300

    Alteração no Netbeans

--- src/exemplogit/ExemploGit.java
+++ src/exemplogit/ExemploGit.java
@@ -15,6 +15,7 @@ public class ExemploGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello Word");
         System.out.println("Exemplo projeto Netbeas e Git");
         System.out.println("Exemplo GitHub");
         // TODO code application logic here
